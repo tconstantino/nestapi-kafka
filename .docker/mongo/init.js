3338aa5da491ef710dbabbd97fb272f7ad2c722f
Thiago Barreto Constantino <thiago.constantino@MACBOOK-WILL130976.local>
2022-09-29 11:18:20 -0300

    API em NestJs fazendo integracao com Kafka

---
+++ .docker/mongo/init.js
@@ -0,0 +1,20 @@
+db.routes.insertMany([
+    {
+        _id: '1',
+        title: 'Primeiro',
+        startPosition: { lat: -15.85594, lng: -47.92923 },
+        endPosition: { lat: -15.82942, lng: -47.92765 }
+    },
+    {
+        _id: '2',
+        title: 'Segundo',
+        startPosition: { lat: -15.82449, lng: -47.92756 },
+        endPosition: { lat: -15.82372, lng: -47.92621 }
+    },
+    {
+        _id: '3',
+        title: 'Terceiro',
+        startPosition: { lat: -15.82331, lng: -47.92558 },
+        endPosition: { lat: -15.82758, lng: -47.92532 }
+    },
+]);
